6e438fe2199f81eb669893184e95135446ce7f2c
tacherasasi <tacherasasi@gmail.com>
2025-04-23 18:06:11 +0300

    Implement email template for custom emails; update ConsoleController to use the new template for sending emails.

--- app/Http/Controllers/ConsoleController.php
+++ app/Http/Controllers/ConsoleController.php
@@ -7,13 +7,11 @@ use Illuminate\Support\Facades\Mail;
 
 class ConsoleController extends Controller
 {
-    // Show the email form
     public function showEmailForm()
     {
         return view('console.email-form');
     }
 
-    // Send email to all users
     public function sendEmailToAllUsers(Request $request)
     {
         $request->validate([
@@ -24,7 +22,10 @@ class ConsoleController extends Controller
         $users = User::all();
 
         foreach ($users as $user) {
-            Mail::raw($request->message, function ($mail) use ($user, $request) {
+            Mail::send('emails.custom', [
+                'subject' => $request->subject,
+                'messageContent' => $request->message
+            ], function ($mail) use ($user, $request) {
                 $mail->to($user->email)
                      ->subject($request->subject);
             });
