7869d734abba163d6d933a5878e73f44f6f59439
tacherasasi <tacherasasi@gmail.com>
2025-04-23 10:50:53 +0300

    Refactor email form and insights write view for improved styling and consistency; update input classes and structure for better responsiveness

--- resources/views/console/email-form.blade.php
+++ resources/views/console/email-form.blade.php
@@ -1,23 +1,29 @@
-<?php
-@extends('layouts.app')
+<x-app-layout>
+    <x-card>
+        <h1 class="text-2xl font-bold">Send Email to All Users</h1>
+        @if(session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+        @endif
+        <form action="{{ route('console.email.send') }}" method="POST">
+            @csrf
+            <div class="mt-2">
+                <x-input-label for="subject" :value="__('Subject')" />
+                <x-text-input id="subject" name="subject" type="text"
+                    class="mt-1 block w-full focus:outline-none focus:border-none focus:ring focus:ring-neutral-800 dark:focus:ring-neutral-400"
+                    :value="old('subject')" required autofocus autocomplete="subject" />
+                <x-input-error class="mt-2" :messages="$errors->get('subject')" />
+            </div>
+            <div class="mt-2">
+                <label class="block font-bold" for="message">Message</label>
+                <x-input-error class="mt-2" :messages="$errors->get('message')" />
+                <textarea name="message" id="message"
+                    class="form-textarea w-full p-4 rounded-lg border dark:bg-neutral-900 dark:border-neutral-700 focus:outline-none focus:border-none focus:ring focus:ring-neutral-800 dark:focus:ring-neutral-400 placeholder-neutral-500 dark:placeholder-neutral-400"
+                    rows="8" required>{{ old('message') }}</textarea>
+            </div>
 
-@section('content')
-<div class="container">
-    <h1>Send Email to All Users</h1>
-    @if(session('success'))
-        <div class="alert alert-success">{{ session('success') }}</div>
-    @endif
-    <form action="{{ route('console.email.send') }}" method="POST">
-        @csrf
-        <div class="mb-3">
-            <label for="subject" class="form-label">Subject</label>
-            <input type="text" name="subject" id="subject" class="form-control" required>
-        </div>
-        <div class="mb-3">
-            <label for="message" class="form-label">Message</label>
-            <textarea name="message" id="message" class="form-control" rows="5" required></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Send Emails</button>
-    </form>
-</div>
-@endsection
+            <div class="mt-2">
+                <x-primary-button>Send</x-primary-button>
+            </div>
+        </form>
+    </x-card>
+</x-app-layout>
